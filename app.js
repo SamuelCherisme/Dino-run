5b89c208774ae3569c9f8d5dd3bef6077874a98f
Samuel Cherisme <ssscherisme@gmail.com>
2020-08-03 18:01:49 -0400

    Fixed javascript wasn't reloading properlye

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-var counter =0;
-
+var counter=0;
 function jump(){
-    if (character.classList == "animate"){return}
+    if(character.classList == "animate"){return}
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
     },300);
 }
-var checkDead = setInterval(function(){
+var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation ="none";
-        alert("Game Over player score: "+Math.floor(counter/100));
-        counter =0;
-        block.style.animation = "block ls infinite linear";
+        block.style.animation = "none";
+        alert("Game Over player 1 score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
     }else{
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
-
-
-},10);
+}, 10);
 
 
 $(document).ready(function(){
