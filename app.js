950b445e0fb438d782d6098fcf392ee73f994fb6
Samuel Cherisme <ssscherisme@gmail.com>
2020-08-04 22:35:21 -0400

    Fixed the character display yessiiirr

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
-var counter=0;
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+let counter=0;
+
 function jump(){
     if(character.classList == "animate"){return}
     character.classList.add("animate");
@@ -8,7 +9,7 @@ function jump(){
         character.classList.remove("animate");
     },300);
 }
-var checkDead = setInterval(function() {
+let checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
